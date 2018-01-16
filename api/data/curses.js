b2944f3dcc1d493e0bb6a9cb4c72d90a66c5d023
Ivan Villa <trezeguet55@gmail.com>
2018-01-15 19:47:32 -0500

    <data>
    fake data

---
+++ api/data/curses.js
@@ -0,0 +1,9 @@
+const curses = [
+  {
+    name: 'Graphql',
+    description: 'Curso de graphql',
+    status: 'ACTIVE'
+  }
+];
+
+module.exports = curses;
---
+++ api/data/data.js
@@ -0,0 +1,11 @@
+const data = [
+  {
+    name: 'Ivan',
+    lastName: 'Villa',
+    address: 'calle 100 #67-28',
+    document: '12345',
+    status: 'ACTIVE'
+  }
+];
+
+module.exports = data;
