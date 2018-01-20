06a702eb24b280d6818a3963514d6f063db10198
Ivan Villa <trezeguet55@gmail.com>
2018-01-19 20:14:22 -0500

    <courses schema.js>
    refactor schema

--- api/courses/schema.js
+++ api/courses/schema.js
@@ -1,15 +1,46 @@
 const schema = [`
   
+  # data to create course
+  input CourseInput {
+    name: String!,
+    description: String,
+    level: Int!,
+    price: Float!,
+    status: Boolean
+  }
+  
+  # data to edit course
+  input CourseEditInput {
+    name: String,
+    description: String,
+    level: Int,
+    price: Float,
+    status: Boolean
+  }
+  
   # data course
   type Course {
     name: String,
     description: String,
-    status: STATUS
+    status: Boolean,
+    price: Float,
+    level: Int
   }
   
-  type Query {
+  extend type Query {
     # find all courses
     courses: [Course]
+    # find course by id
+    courseById(id: ID!): Course
+  }
+  
+  extend type Mutation {
+    # create course
+    courseAdd(data: CourseInput): Course
+    # update course
+    courseEdit(id: ID!, data: CourseEditInput): Course
+    # delete course
+    courseDelete(id: ID!): Course
   }
 `];
 
