c0929af95307d7730351a3d936bf8e7b23d98612
Ivan Villa <trezeguet55@gmail.com>
2018-01-19 20:15:28 -0500

    <courses resolver.js>
    create graphql resolver

--- api/courses/resolver.js
+++ api/courses/resolver.js
@@ -1,7 +1,52 @@
 const resolver = {
   Query: {
-    courses(root, args, {curses}) {
-      return curses;
+    async courses(root, args, {courseStorage}) {
+      let results = [];
+      try {
+        results = await courseStorage.find({});
+      } catch (err) {
+        console.log('err', err);
+        return results;
+      }
+      return results;
+    },
+    async courseById(root, args, {courseStorage}) {
+      let result = {};
+      try {
+        result = await courseStorage.findById(args.id);
+      } catch (err) {
+        return result;
+      }
+      return result;
+    }
+  },
+  Mutation: {
+    async courseAdd(root, args, {courseStorage}) {
+      let result = {};
+      try {
+        result = await courseStorage.save({...args.data});
+      } catch (err) {
+        return result;
+      }
+      return result;
+    },
+    async courseEdit(root, args, {courseStorage}) {
+      let result = {};
+      try {
+        result = await courseStorage.update(args.id, {...args.data});
+      } catch (err) {
+        return result;
+      }
+      return result;
+    },
+    async courseDelete(root, args, {courseStorage}) {
+      let result = {};
+      try {
+        result = await courseStorage.delete(args.id);
+      } catch (err) {
+        return result;
+      }
+      return result;
     }
   }
 };
