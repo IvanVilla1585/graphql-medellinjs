675560ff32155243d5dc2d7b445a5d2b5055d9fb
Ivan Villa <trezeguet55@gmail.com>
2018-01-19 20:16:02 -0500

    <teacher resolver.js>
    create graphql resolver

--- api/teacher/resolver.js
+++ api/teacher/resolver.js
@@ -1,7 +1,52 @@
 const resolver = {
   Query: {
-    teachers(root, args, {data}) {
-      return data;
+    async teachers(root, args, {teacherStorage}) {
+      let results = [];
+      try {
+        results = await teacherStorage.find({});
+      } catch (err) {
+        console.log('err', err);
+        return results;
+      }
+      return results;
+    },
+    async teacherById(root, args, {teacherStorage}) {
+      let result = {};
+      try {
+        result = await teacherStorage.findById(args.id);
+      } catch (err) {
+        return result;
+      }
+      return result;
+    }
+  },
+  Mutation: {
+    async teacherAdd(root, args, {teacherStorage}) {
+      let result = {};
+      try {
+        result = await teacherStorage.save(args.data);
+      } catch (err) {
+        return result;
+      }
+      return result;
+    },
+    async teacherEdit(root, args, {teacherStorage}) {
+      let result = {};
+      try {
+        result = await teacherStorage.update(args.id, args.data);
+      } catch (err) {
+        return result;
+      }
+      return result;
+    },
+    async teacherDelete(root, args, {teacherStorage}) {
+      let result = {};
+      try {
+        result = await teacherStorage.delete(args.id);
+      } catch (err) {
+        return result;
+      }
+      return result;
     }
   }
 };
