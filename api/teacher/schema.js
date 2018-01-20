361dcba9b41a48a71f0762cb635185117ccce2ff
Ivan Villa <trezeguet55@gmail.com>
2018-01-19 20:16:34 -0500

    <teacher schema.js>
    create graphql schema

--- api/teacher/schema.js
+++ api/teacher/schema.js
@@ -1,21 +1,60 @@
 const schema = [`
-  enum STATUS {
-    ACTIVE,
-    INACTIVE
+  enum TYPE {
+    TEACHER,
+    STUDENT
   }
   
-  # data teacher
-  type Teacher {
+  # data to create teacher
+  input TeacherInput {
+    name: String!,
+    lastName: String!,
+    address: String,
+    document: String!,
+    documentType: String!,
+    type: TYPE!,
+    status: Boolean,
+    courses: [ID]
+  }
+  
+  # data to edit teacher
+  input TeacherEditInput {
     name: String,
     lastName: String,
     address: String,
     document: String,
-    status: STATUS
+    documentType: String,
+    type: TYPE,
+    status: Boolean,
+    courses: [ID]
+  }
+  
+  # data teacher
+  type Teacher {
+    _id: ID,
+    name: String,
+    lastName: String,
+    address: String,
+    document: String,
+    documentType: String,
+    type: TYPE,
+    status: Boolean,
+    courses: [Course]
   }
   
   extend type Query {
     # find all teachers
     teachers: [Teacher]
+    # find teacher by id
+    teacherById(id: ID!): Teacher
+  }
+  
+  extend type Mutation {
+    # create teacher
+    teacherAdd(data: TeacherInput): Teacher
+    # update teacher
+    teacherEdit(id: ID!, data: TeacherEditInput): Teacher
+    # delete teacher
+    teacherDelete(id: ID!): Teacher
   }
 `];
 
