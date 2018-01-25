d0992b0e25f78474c9d2130abc92d1fc4ad1cdb7
Ivan Villa <trezeguet55@gmail.com>
2018-01-24 23:34:12 -0500

    <post resolver.js>
    refactor code

--- api/graphql/post/resolver.js
+++ api/graphql/post/resolver.js
@@ -1,19 +1,51 @@
 const resolver = {
   Query: {
-    async courses(root, args, {courseStorage}) {
+    async posts(root, args, {postStorage}) {
       let results = [];
       try {
-        results = await courseStorage.find({});
+        results = await postStorage.find();
       } catch (err) {
         console.log('err', err);
         return results;
       }
       return results;
     },
-    async courseById(root, args, {courseStorage}) {
+    async search(root, args, {postStorage, commentStorage}) {
+      let results = [];
+      try {
+        const resultsPost = await postStorage.find({body: {$regex: new RegExp(args.search, 'i')}});
+        const resultsComments = await commentStorage.find({body: {$regex: new RegExp(args.search, 'i')}});
+        results = [...resultsPost, ...resultsComments]
+      } catch (err) {
+        console.log('err', err);
+        return results;
+      }
+      return results;
+    },
+    async postById(root, args, {postStorage}) {
       let result = {};
       try {
-        result = await courseStorage.findById(args.id);
+        result = await postStorage.findById(args.id);
+      } catch (err) {
+        return result;
+      }
+      return result;
+    }
+  },
+  Post: {
+    async user({userId}, args, {userStorage}) {
+      let result = {};
+      try {
+        result = await userStorage.findById(userId);
+      } catch (err) {
+        return result;
+      }
+      return result;
+    },
+    async comments({id}, args, {commentStorage}) {
+      let result = [];
+      try {
+        result = await commentStorage.find({postId: id});
       } catch (err) {
         return result;
       }
@@ -21,28 +53,10 @@ const resolver = {
     }
   },
   Mutation: {
-    async courseAdd(root, args, {courseStorage}) {
+    async postAdd(root, args, {postStorage}) {
       let result = {};
       try {
-        result = await courseStorage.save({...args.data});
-      } catch (err) {
-        return result;
-      }
-      return result;
-    },
-    async courseEdit(root, args, {courseStorage}) {
-      let result = {};
-      try {
-        result = await courseStorage.update(args.id, {...args.data});
-      } catch (err) {
-        return result;
-      }
-      return result;
-    },
-    async courseDelete(root, args, {courseStorage}) {
-      let result = {};
-      try {
-        result = await courseStorage.delete(args.id);
+        result = await postStorage.save({...args.data});
       } catch (err) {
         return result;
       }
