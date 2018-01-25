b543359de2cd43c86c0c0818e0b8554ba53daaff
Ivan Villa <trezeguet55@gmail.com>
2018-01-25 00:06:58 -0500

    <graphql schema.js>
    refactor code

--- api/graphql/schema.js
+++ api/graphql/schema.js
@@ -2,21 +2,21 @@ const merge = require('lodash.merge');
 const {makeExecutableSchema} = require('graphql-tools');
 
 // import schemas and resolvers
-const teacherSchema = require('./graphql/user/schema');
-const teacherResolver = require('./graphql/user/resolver');
-const cursesSchema = require('./graphql/post/schema');
-const cursesResolver = require('./graphql/post/resolver');
-const studentSchema = require('./graphql/comment/schema');
-const studentResolver = require('./graphql/comment/resolver');
+const userSchema = require('./user/schema');
+const userResolver = require('./user/resolver');
+const postSchema = require('./post/schema');
+const postResolver = require('./post/resolver');
+const commentSchema = require('./comment/schema');
+const commentResolver = require('./comment/resolver');
 
 const typeDefs = [
-  ...teacherSchema,
-  ...cursesSchema,
-  ...studentSchema
+  ...userSchema,
+  ...postSchema,
+  ...commentSchema
 ];
 
 const resolvers = merge(
-    teacherResolver, cursesResolver, studentResolver
+    userResolver, postResolver, commentResolver
 );
 
 
